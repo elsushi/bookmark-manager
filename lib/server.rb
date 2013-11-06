1bdcf72bbf013dacc10f44cde29d68f620fb2c8d
Arturo Ortiz <elsushi@gmail.com>
2013-11-06 12:20:29 +0000

    Adding tags to links

--- lib/server.rb
+++ lib/server.rb
@@ -5,6 +5,8 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
+require './lib/tag'
+
 
 DataMapper.finalize
 
