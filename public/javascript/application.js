f9f53c3fac19f1812d95987f7652c54e422fbecc
Arturo Ortiz <elsushi@gmail.com>
2013-11-15 18:35:08 +0000

    Flashes

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,18 @@
+function showLinkFavouritedNotice(link) {
+  var favourited = !!$(link).data("favourited");
+  // get the text of the .title element
+  // that we find among the children of link
+  var name = $(link).find('.title').text();
+  var message = favourited ?
+                name + " was added to favourites" :
+                name + " was removed from favourites";
+  var flash = $("<div></div>").addClass('flash').addClass('notice').html(message);
+  $(flash).appendTo('#flash-container');
+  setTimeout(function() {
+    $(flash).fadeOut();
+  }, 3000);
+}
+
 function addFavouritesHandler() {
 	$('.star.solid').click(function() {
 		var link = $(this).parent();
@@ -5,9 +20,12 @@ function addFavouritesHandler() {
 		var newOpacity = favourited ? 0 : 1;
 		$(link).data("favourited", !favourited);
 	$(this).animate({opacity: newOpacity}, 1000);
+	showLinkFavouritedNotice(link);
 	});
 }
 
 $(function() {
 	addFavouritesHandler();
 })
+
+
